8531a3bf9ed7bb786a3be8458bff8a0501c4d1f9
Manuel <msalaguila@gmail.com>
2019-09-24 10:55:59 +0100

    Initial commit

---
+++ app/src/main/java/pem/tema4/presentador/IPresentadorPrincipal.java
@@ -0,0 +1,15 @@
+package pem.tema4.presentador;
+
+public interface IPresentadorPrincipal {
+
+	// TODO Añadir el método obtenerDatos() de tipo void que solicita los datos de la lista maestro al modelo.
+
+    public void obtenerDatos();
+	// TODO Añadir el método obtenerDetalles() que recupera los datos de una receta dada su posición
+	// en la lista maestro.
+
+    public void obtenerDetalles();
+	// TODO Añadir el método tratarAgregar() que lanza la vista de agregación por medio del mediador.
+
+
+}
